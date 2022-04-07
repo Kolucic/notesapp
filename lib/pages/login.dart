9bbdef9ce8a3e296da2cd77c630fde159632ace2
kolucic <mogjot@gmail.com>
2022-04-08 00:04:38 +0200

    Ridisegnata interfaccia login e abiilitato login automatico

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,87 +1,171 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:noteapp/controller/google_auth.dart';
+import 'package:noteapp/utils/color_constant.dart';
+import 'package:noteapp/utils/math_utils.dart';
 
 class LoginPage extends StatefulWidget {
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      "assets/images/note-logo.png",
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            //
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 10.0,
-                vertical: 12.0,
-              ),
-              child: Text(
-                "Crea e gestisci le tue note",
-                style: TextStyle(
-                    fontSize: 36.0,
-                    fontFamily: "lato",
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 10.0,
-              ),
-              child: ElevatedButton(
-                onPressed: () {
-                  signInWithGoogle(context);
-                },
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Continua con Google",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontFamily: "lato",
+    signInWithGoogle(context);
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: ColorConstant.whiteA700,
+        body: Container(
+          decoration: BoxDecoration(
+            color: ColorConstant.whiteA700,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: SingleChildScrollView(
+                    padding: EdgeInsets.only(
+                      top: getVerticalSize(
+                        292.00,
+                      ),
+                      bottom: getVerticalSize(
+                        20.00,
                       ),
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Image.asset(
-                      'assets/images/google-logo.png',
-                      height: 36.0,
-                    ),
-                  ],
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                    Colors.grey[700],
-                  ),
-                  padding: MaterialStateProperty.all(
-                    EdgeInsets.symmetric(
-                      vertical: 12.0,
-                    ),
+                    child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: getHorizontalSize(
+                                41.00,
+                              ),
+                              right: getHorizontalSize(
+                                41.00,
+                              ),
+                            ),
+                            child: Text(
+                              "Cloud note",
+                              overflow: TextOverflow.ellipsis,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: ColorConstant.black900,
+                                fontSize: getFontSize(
+                                  24,
+                                ),
+                                fontFamily: 'Red Hat Display',
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: getHorizontalSize(
+                              278.00,
+                            ),
+                            margin: EdgeInsets.only(
+                              left: getHorizontalSize(
+                                41.00,
+                              ),
+                              top: getVerticalSize(
+                                30.00,
+                              ),
+                              right: getHorizontalSize(
+                                41.00,
+                              ),
+                            ),
+                            child: Text(
+                              "Crea e gestisci le tue note collegandole al tuo account Google.",
+                              maxLines: null,
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: ColorConstant.black900,
+                                fontSize: getFontSize(
+                                  12,
+                                ),
+                                fontFamily: 'Red Hat Text',
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(
+                              left: getHorizontalSize(
+                                41.00,
+                              ),
+                              top: getVerticalSize(
+                                47.00,
+                              ),
+                              right: getHorizontalSize(
+                                41.00,
+                              ),
+                            ),
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: getVerticalSize(
+                                48.00,
+                              ),
+                              width: getHorizontalSize(
+                                278.00,
+                              ),
+                              decoration: BoxDecoration(
+                                color: ColorConstant.orange700,
+                                borderRadius: BorderRadius.circular(
+                                  getHorizontalSize(
+                                    16.00,
+                                  ),
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: ColorConstant.orange50,
+                                    spreadRadius: getHorizontalSize(
+                                      2.00,
+                                    ),
+                                    blurRadius: getHorizontalSize(
+                                      2.00,
+                                    ),
+                                    offset: Offset(
+                                      0,
+                                      4,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  signInWithGoogle(context);
+                                },
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Continua con Google",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: ColorConstant.whiteA700,
+                                        fontSize: getFontSize(
+                                          14,
+                                        ),
+                                        fontFamily: 'Red Hat Text',
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ]),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
