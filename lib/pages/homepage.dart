e31a72dc385e2adbec765923c0b764c7a3320598
kolucic <mogjot@gmail.com>
2022-04-08 17:31:12 +0200

    Aggiunta didascalia sotto al titolo della nota Aggiornata ui relativa alla visualizzazione delle note

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -8,6 +8,9 @@ import 'package:intl/intl.dart';
 import 'package:noteapp/pages/addNote.dart';
 import 'package:noteapp/pages/viewNote.dart';
 
+import '../utils/color_constant.dart';
+import '../utils/math_utils.dart';
+
 class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
@@ -21,131 +24,174 @@ class _HomePageState extends State<HomePage> {
 
   List<Color> myColors = [
     Colors.orange[100],
-    Colors.red[200],
-    Colors.grey[350],
-    Colors.deepPurple[200],
+    Colors.red[100],
+    Colors.yellow[100],
+    Colors.brown[100],
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context)
-              .push(
-            MaterialPageRoute(
-              builder: (context) => AddNote(),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: ColorConstant.whiteA700,
+
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.of(context)
+                .push(
+              MaterialPageRoute(
+                builder: (context) => AddNote(),
+              ),
+            )
+                .then((value) {
+              print("Calling a set state");
+              setState(() {});
+            });
+          },
+          label: Text(
+            "Aggiungi una nota",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: ColorConstant.whiteA700,
+              fontSize: getFontSize(
+                14,
+              ),
+              fontFamily: 'Red Hat Text',
+              fontWeight: FontWeight.w500,
             ),
-          )
-              .then((value) {
-            print("Calling a set state");
-            setState(() {});
-          });
-        },
-        child: Icon(
-          Icons.add,
-          color: Colors.white70,
-        ),
-        backgroundColor: Colors.grey[700],
-      ),
-
-      //
-
-      appBar: AppBar(
-        title: Text(
-          'Notes',
-          style: TextStyle(
-            fontSize: 32.0,
-            fontFamily: "lato",
-            fontWeight: FontWeight.bold,
-            color: Colors.white70,
           ),
+          backgroundColor: ColorConstant.orange700,
         ),
-        elevation: 0.0,
-        backgroundColor: Color(0xff070706),
-      ),
 
-      //
+        //
+        appBar: AppBar(
+          title: Text(
+            'Le mie note',
+            style: TextStyle(
+              fontSize: 24.0,
+              overflow: TextOverflow.ellipsis,
+              fontFamily: "Red Hat Display",
+              fontWeight: FontWeight.w700,
+              color: ColorConstant.black900,
+            ),
+          ),
+          elevation: 0.0,
+          backgroundColor: ColorConstant.whiteA700,
+        ),
 
-      body: FutureBuilder<QuerySnapshot>(
-        future: ref.get(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            if (snapshot.data.docs.length == 0) {
-              return Center(
-                child: Text(
-                  "Non hai note !",
-                  style: TextStyle(
-                    color: Colors.white70,
-                  ),
-                ),
-              );
-            }
-            return ListView.builder(
-              itemCount: snapshot.data?.docs.length,
-              itemBuilder: (context, index) {
-                Random random = new Random();
-                Color bg = myColors[random.nextInt(4)];
-                Map data = snapshot.data.docs[index].data();
-                DateTime mydateTime = data['created'].toDate();
-                String formattedTime =
-                    DateFormat.yMMMd().add_jm().format(mydateTime);
-                return InkWell(
-                  onTap: () {
-                    Navigator.of(context)
-                        .push(
-                      MaterialPageRoute(
-                        builder: (context) => ViewNote(
-                          data,
-                          formattedTime,
-                          snapshot.data.docs[index].reference,
-                        ),
-                      ),
-                    )
-                        .then((value) {
-                      setState(() {});
-                    });
-                  },
-                  child: Card(
-                    color: bg,
-                    child: Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "${data['title']}",
-                            style: TextStyle(
-                              fontSize: 24.0,
-                              fontFamily: "lato",
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black87,
-                            ),
-                          ),
-                          Container(
-                            alignment: Alignment.centerRight,
-                            child: Text(
-                              formattedTime,
-                              style: TextStyle(
-                                fontSize: 20.0,
-                                fontFamily: "lato",
-                                color: Colors.black87,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+        //
+        body: FutureBuilder<QuerySnapshot>(
+          future: ref.get(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data.docs.length == 0) {
+                return Center(
+                  child: Text(
+                    "Non hai note !",
+                    style: TextStyle(
+                      color: Colors.white70,
                     ),
                   ),
                 );
-              },
-            );
-          } else {
-            return Center(
-              child: Text("Loading..."),
-            );
-          }
-        },
+              }
+              return ListView.builder(
+                itemCount: snapshot.data?.docs.length,
+                itemBuilder: (context, index) {
+                  Random random = new Random();
+                  Color bg = myColors[random.nextInt(4)];
+                  Map data = snapshot.data.docs[index].data();
+                  DateTime mydateTime = data['created'].toDate();
+                  String formattedTime =
+                      DateFormat.yMMMd().add_jm().format(mydateTime);
+                  return InkWell(
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(
+                        MaterialPageRoute(
+                          builder: (context) => ViewNote(
+                            data,
+                            formattedTime,
+                            snapshot.data.docs[index].reference,
+                          ),
+                        ),
+                      )
+                          .then((value) {
+                        setState(() {});
+                      });
+                    },
+                    child: Card(
+                      color: bg,
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            left: getHorizontalSize(20.00),
+                            top: getVerticalSize(17.00),
+                            right: getHorizontalSize(20.00)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                  getHorizontalSize(
+                                    16.00,
+                                  ),
+                                ),
+                              ),
+                              child: Text(
+                                "${data['title']}",
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  fontFamily: "Red Hat Display",
+                                  fontWeight: FontWeight.w600,
+                                  color: ColorConstant.black900,
+                                ),
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  child: Text(
+                                    "${data['description']}",
+                                    style: TextStyle(
+                                      fontSize: 12.0,
+                                      fontFamily: "Red Hat Text",
+                                      color: ColorConstant.black900,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                formattedTime,
+                                style: TextStyle(
+                                  fontSize: 12.0,
+                                  fontFamily: "Red Hat Text",
+                                  color: ColorConstant.black900,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            } else {
+              return Center(
+                child: Text("Loading..."),
+              );
+            }
+          },
+        ),
       ),
     );
   }
